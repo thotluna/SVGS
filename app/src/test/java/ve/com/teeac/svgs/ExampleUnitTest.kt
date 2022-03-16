e45c6e74163051944b165079da7666640d29140d
thotluna <eladi.feijoo@gmail.com>
2022-03-16 17:27:07 -0400

    Ktlint configuration

--- app/src/test/java/ve/com/teeac/svgs/ExampleUnitTest.kt
+++ app/src/test/java/ve/com/teeac/svgs/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ve.com.teeac.svgs
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
