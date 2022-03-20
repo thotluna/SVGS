d1483e890fc1d15b4c129b0a461073f8ed2223f7
thotluna <eladi.feijoo@gmail.com>
2022-03-20 19:57:15 -0400

    Hilt dagger Implementation

---
+++ app/src/main/java/ve/com/teeac/svgs/di/AppModule.kt
@@ -0,0 +1,17 @@
+
+package ve.com.teeac.svgs.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providerTestText() = "Here test text"
+}
