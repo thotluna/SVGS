1a3da5b484d428c843e6b4a4c78224d3e76af0ee
thotluna <eladi.feijoo@gmail.com>
2022-04-14 09:52:19 -0400

    Repair Refactoring

---
+++ app/src/main/java/ve/com/teeac/svgs/authentication/domain/use_case/SignInTwitterUseCase.kt
@@ -0,0 +1,16 @@
+package ve.com.teeac.svgs.authentication.domain.use_case
+
+import android.app.Activity
+import ve.com.teeac.svgs.authentication.data.data_source.AuthenticationOAuthByFirebase
+import ve.com.teeac.svgs.authentication.data.models.User
+import ve.com.teeac.svgs.di.AuthTwitter
+import javax.inject.Inject
+
+class SignInTwitterUseCase @Inject constructor(
+    @AuthTwitter private var auth: AuthenticationOAuthByFirebase
+) {
+
+    suspend operator fun invoke(activity: Activity): User? {
+        return auth.signIn(activity = activity)
+    }
+}
